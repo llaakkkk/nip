b29335e58d5c2fdc301ed905210cceb9ffcd33a3
llaakkkk <lenakirichok@gmail.com>
2016-01-29 11:36:07 +0200

    Update form.blade.php

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -1,9 +1,9 @@
 <form class="form-horizontal" role="form"  action="/currency" method="post" enctype="multipart/form-data">
     <input type="hidden" name="_token" value="{!! csrf_token() !!}">
     <div class="form-group">
-        <label for="inputUser" class="col-sm-4 control-label">Input Your Sum *</label>
+        <label for="inputUser" class="col-sm-4 control-label">Enter Your Sum *</label>
         <div class="col-sm-8">
-            <input type="text" name="inputUser" class="form-control" id="inputUser" placeholder="Input Sum">
+            <input type="text" name="inputUser" class="form-control" id="inputUser" placeholder="Enter Sum">
         </div>
     </div>
     <div class="form-group">
